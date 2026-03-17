473ef1f1c1b6517344a32126c953934c15d59e47
Manish <manishvm310@gmail.com>
2026-03-17 16:11:02 +0530

    Commit-v1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -712,9 +712,9 @@ function Footer() {
 /* ---------------- IMAGE SLIDER COMPONENT ---------------- */
 function ImageSlider() {
   const images = [
-    '../banner-1.png',
-    '../banner-2.png',
-    'inventory.png'
+    'https://github.com/manish-vm/Inventory-Management-Frontend/blob/main/banner-1.png?raw=true',
+    'https://github.com/manish-vm/Inventory-Management-Frontend/blob/main/banner-2.png?raw=true',
+    'https://github.com/manish-vm/Inventory-Management-Frontend/blob/main/inventory.png?raw=true'
   ];
   const [currentIndex, setCurrentIndex] = useState(0);
 
