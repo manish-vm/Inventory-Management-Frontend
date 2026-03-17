720e474ab1790dcb1e83e09b71e5440a6cda17e8
Manish <manishvm310@gmail.com>
2026-03-17 17:38:55 +0530

    Commit-v1

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://inventory-management-backend-k76m.onrender.com/api";
 
 // Create axios instance
 const api = axios.create({
